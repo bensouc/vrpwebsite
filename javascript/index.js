74ea4809ebfdb39adf57665aeb7034ed42024328
benoit moret <benoit.moret@gmail.com>
2023-10-11 13:18:12 +0200

    darkmode

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,9 @@
-
+const darkModeBtn = document.getElementById("modeBtn")
+darkModeBtn.addEventListener("click",()=>{
+  console.log("change mode")
+  const elements = document.querySelectorAll('*');
+  elements.forEach(element => {
+    element.classList.toggle("dark-mode")
+    // element.classList.toggle("light-mode")
+  })
+})
